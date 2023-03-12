3a1f4457872bc25df1bab52b8aa861e98b840b98
m4st3 <runtycybin@gmail.com>
2023-03-12 20:53:39 +0100

    UserService moved to package and refactored

---
+++ src/main/java/com/cybin/cybinOrg/service/UserService.java
@@ -0,0 +1,32 @@
+package com.cybin.cybinOrg.service;
+
+import com.cybin.cybinOrg.dto.UserRequestDTO;
+import com.cybin.cybinOrg.entity.Role;
+import com.cybin.cybinOrg.entity.User;
+import com.cybin.cybinOrg.repository.RoleRepo;
+import com.cybin.cybinOrg.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private UserRepo userRepo;
+    private RoleRepo roleRepo;
+
+    @Autowired
+    public UserService(UserRepo repoUser, RoleRepo repoRol) {
+        this.userRepo = repoUser;
+        this.roleRepo = repoRol;
+    }
+
+    public User createUser(UserRequestDTO usr) {
+        User user = new User();
+        Role role = roleRepo.findById(usr.getRoleId()).get();
+        user.setRoleId(role);
+        user.setUserName(usr.getUsername());
+
+        return userRepo.save(user);
+    }
+
+}
